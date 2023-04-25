9646e35f82bcf12ce47523c20bbe21293f048a02
IgorAgarkov <agarkov.ig@mail.ru>
2023-04-25 15:10:54 +0700

    PdfFileMerger -> PdfMerger

--- pdf_file_merger.py
+++ pdf_file_merger.py
@@ -1,12 +1,12 @@
 # -*- coding: utf8 -*-
-from PyPDF2 import PdfFileMerger 
+from PyPDF2 import PdfMerger# PdfFileMerger 
 import os
 files = os.listdir()
 pdfs = [] 
 for pdf in files:
     if pdf[-4:].lower() == '.pdf':
         pdfs.append(pdf)
-merger = PdfFileMerger() 
+merger = PdfMerger() 
 for pdf in pdfs:
     merger.append(pdf) 
 merger.write("!_Чертежи PDF (все в одном).pdf") 
